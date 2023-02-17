2766ab9c79db2404e9724555b651272b8a287887
juggler108 <juggler108@gmail.com>
2023-02-17 14:28:24 +0400

    created final test project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # stepik_auto_test_course_main_project
+
+### Final test project of "auto_test_course" 
---
+++ conftest.py
@@ -0,0 +1,45 @@
+from time import sleep
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en", help="Choose the language!")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    user_language = request.config.getoption("language")
+
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+
+    print("\nstart chrome browser for test..")
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(3)
+
+    driver.get(link)
+    sleep(5)
+    yield driver
+
+    print("\nquit browser..")
+    driver.quit()
+
+
+# @pytest.fixture(scope="function")
+# def browser(request):
+#     language = request.config.getoption("language")
+#     link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
+#
+#     print("\nstart chrome browser for test..")
+#     driver = webdriver.Chrome()
+#     driver.implicitly_wait(3)
+#
+#     driver.get(link)
+#     sleep(10)
+#     yield driver
+#
+#     print("\nquit browser..")
+#     driver.quit()
